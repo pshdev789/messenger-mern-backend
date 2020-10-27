eeeaa7127452438a916c2706fd735954b4380aad
pshdev789 <68139465+pshdev789@users.noreply.github.com>
2020-10-27 07:26:38 +0530

    Building the basic structure of a node project.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-// import dependencies
+import express from 'express'
+import mongoose from 'mongoose'
+import Pusher from 'pusher'
+import cors from 'cors'
 
 // App config
+const app = express();
+const port = process.env.PORT || 9000;
+
+
 
 // Middleware
 
@@ -8,5 +15,7 @@
 // DB Config
 
 // API Routes
+app.get('/',(req,res)=> res.status(200).send('hello World'));
 
 // Listen
+app.listen(port, ()=>console.log(`listening on: ${port}`))
